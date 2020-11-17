1f51e570fd808608c39de7fbe7c6762d4273e19b
Wilbert Guo <wilbeguo@amazon.com>
2020-11-17 11:08:44 -0800

    Add AWS X-Ray ID Generator and unit tests

---
+++ idgenerator/aws/aws_xray_idgenerator.go
@@ -0,0 +1,84 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS'" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
+
+package aws
+
+import (
+	crand "crypto/rand"
+	"encoding/binary"
+	"encoding/hex"
+	"math/rand"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type spanID [8]byte
+type traceID [16]byte
+
+type idGenerator interface {
+	NewTraceID() traceID
+	NewSpanID() spanID
+}
+
+type xRayIDGenerator struct {
+	sync.Mutex
+	randSource *rand.Rand
+}
+
+func awsXRayIDGenerator() idGenerator {
+	gen := &xRayIDGenerator{}
+	var rngSeed int64
+	_ = binary.Read(crand.Reader, binary.LittleEndian, &rngSeed)
+	gen.randSource = rand.New(rand.NewSource(rngSeed))
+	return gen
+}
+
+// NewSpanID returns a non-zero span ID from a randomly-chosen sequence.
+func (gen *xRayIDGenerator) NewSpanID() spanID {
+	gen.Lock()
+	defer gen.Unlock()
+	sid := spanID{}
+	gen.randSource.Read(sid[:])
+	return sid
+}
+
+// NewTraceID returns a non-zero trace ID based on AWS X-Ray TraceID format.
+// (https://docs.aws.amazon.com/xray/latest/devguide/xray-api-sendingdata.html#xray-api-traceids)
+func (gen *xRayIDGenerator) NewTraceID() traceID {
+	gen.Lock()
+	defer gen.Unlock()
+
+	tid := traceID{}
+	currentTime := getCurrentTimeHex()
+	copy(tid[:4], currentTime)
+	gen.randSource.Read(tid[4:])
+
+	return tid
+}
+
+func getCurrentTimeHex() []uint8 {
+	currentTime := time.Now().Unix()
+	currentTimeHex, err := hex.DecodeString(strconv.FormatInt(currentTime, 16))
+	if err != nil {
+		panic(err)
+	}
+	return currentTimeHex
+}
+
+func intToHex(n int64) []byte {
+	return []byte(strconv.FormatInt(n, 16))
+}
---
+++ idgenerator/aws/aws_xray_idgenerator_test.go
@@ -0,0 +1,118 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS'" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
+
+package aws
+
+import (
+	"bytes"
+	"encoding/hex"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func (t traceID) convertTraceIDToHexString() string {
+	return hex.EncodeToString(t[:])
+}
+
+func (s spanID) cconvertSpanIDToHexString() string {
+	return hex.EncodeToString(s[:])
+}
+
+func TestAwsXRayTraceIdIsValidLength(t *testing.T) {
+	idg := awsXRayIDGenerator()
+	traceIDHex := idg.NewTraceID().convertTraceIDToHexString()
+	traceIDLength := len(traceIDHex)
+
+	if traceIDLength != 32 {
+		t.Errorf("TraceID has incorrect length. Got length of %d, expected 32", traceIDLength)
+	}
+}
+
+func TestAwsXRayTraceIdIsUnique(t *testing.T) {
+	idg := awsXRayIDGenerator()
+	traceID1 := idg.NewTraceID().convertTraceIDToHexString()
+	traceID2 := idg.NewTraceID().convertTraceIDToHexString()
+
+	if traceID1 == traceID2 {
+		t.Errorf("TraceID should be unique. Got TraceID1 = %s and TraceID2 = %s", traceID1, traceID2)
+	}
+}
+
+func TestAwsXRayTraceIdTimeStampInBounds(t *testing.T) {
+
+	idg := awsXRayIDGenerator()
+
+	previousTime := time.Now().Unix()
+
+	traceIDHex := idg.NewTraceID().convertTraceIDToHexString()
+	currentTime, err := strconv.ParseInt(traceIDHex[0:8], 16, 64)
+
+	nextTime := time.Now().Unix()
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	inLowerBound := previousTime <= currentTime
+	inUpperBound := currentTime <= nextTime
+
+	if !inLowerBound || !inUpperBound {
+		t.Errorf("TraceID is generated incorrectly with the wrong timestamp. Got epoch time %d, expected epoch time should be between %d and %d", currentTime, previousTime, currentTime)
+	}
+}
+
+func TestAwsXRayTraceIdIsNotNil(t *testing.T) {
+	var nilTraceID traceID
+	idg := awsXRayIDGenerator()
+	traceID := idg.NewTraceID()
+	isNil := bytes.Equal(traceID[:], nilTraceID[:])
+
+	if isNil == true {
+		t.Error("TraceID cannot be Nil.")
+	}
+}
+
+func TestAwsXRaySpanIdIsValidLength(t *testing.T) {
+	idg := awsXRayIDGenerator()
+	spanIDHex := idg.NewSpanID().cconvertSpanIDToHexString()
+	spanIDLength := len(spanIDHex)
+
+	if spanIDLength != 16 {
+		t.Errorf("SpanID has incorrect length. Got length of %d, expected 16", spanIDLength)
+	}
+}
+
+func TestAwsXRaySpanIdIsUnique(t *testing.T) {
+	idg := awsXRayIDGenerator()
+	spanID1 := idg.NewSpanID().cconvertSpanIDToHexString()
+	spanID2 := idg.NewSpanID().cconvertSpanIDToHexString()
+
+	if spanID1 == spanID2 {
+		t.Errorf("SpanID should be unique. Got spanID1 = %s and spanID2 = %s", spanID1, spanID2)
+	}
+}
+
+func TestAwsXRaySpanIdIsNotNil(t *testing.T) {
+	var nilSpanID spanID
+	idg := awsXRayIDGenerator()
+	spanID := idg.NewSpanID()
+	isNil := bytes.Equal(spanID[:], nilSpanID[:])
+
+	if isNil == true {
+		t.Error("SpanID cannot be Nil.")
+	}
+}
